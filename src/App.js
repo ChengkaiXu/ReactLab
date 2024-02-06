8bec49e022a7ee491591d14d9478a9ac826bd9ac
ChengkaiXu <24cxu@athenian.org>
2024-02-06 10:27:50 -0800

    Extra challenge: Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/App.js
+++ src/App.js
@@ -33,21 +33,18 @@ function Board({xIsNext, squares, onPlay }) {
   return (
       <>
         <div className="status">{status}</div>
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
+        {
+          Array(3).fill(null).map((_, index) => (
+              <div className="board-row" key={index}>
+                {
+                  Array(3).fill(null).map((_, colIndex) => {
+                    const index = index * 3 + colIndex;
+                    return <Square value={squares[index]} onSquareClick={() => handleClick(index)} key={index} />;
+                  })
+                }
+              </div>
+          ))
+        }
       </>
   );
 }
